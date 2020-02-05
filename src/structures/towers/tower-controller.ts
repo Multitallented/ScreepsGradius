172023444955b0cf091b33e667590a05fe4053ac
Multitallented <multitallented@gmail.com>
2020-02-05 15:52:01 -0800

    minor tower energy conservation

--- src/structures/towers/tower-controller.ts
+++ src/structures/towers/tower-controller.ts
@@ -1,14 +1,22 @@
 import * as _ from "lodash";
 
 export class TowerController {
+    static isOnEdge(pos:RoomPosition):boolean {
+        return pos.x < 3 || pos.x > 47 || pos.y < 3 || pos.y > 47;
+    }
+
     static run(room:Room) {
+        let engagedTowers = 0;
         _.forEach(room.find(FIND_MY_STRUCTURES, {filter: (s:Structure) => {
                 return s.structureType === STRUCTURE_TOWER;
             }}), (tower:StructureTower) => {
 
             let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-            if (closestHostile) {
-                tower.attack(closestHostile);
+            if (closestHostile && (engagedTowers < 1 || !TowerController.isOnEdge(closestHostile.pos))) {
+                let attackMessage = tower.attack(closestHostile);
+                if (attackMessage === OK) {
+                    engagedTowers += 1;
+                }
             }
         });
     }
